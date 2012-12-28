48f3c43708839ad696ae9de4bf4af6ea000c6a11
Paul M. Jones <pmjones88@gmail.com>
2012-12-27 19:24:17 -0600

    load the new files

--- src.php
+++ src.php
@@ -2,5 +2,7 @@
 require_once __DIR__ . '/src/Aura/Session/CsrfToken.php';
 require_once __DIR__ . '/src/Aura/Session/CsrfTokenFactory.php';
 require_once __DIR__ . '/src/Aura/Session/Manager.php';
+require_once __DIR__ . '/src/Aura/Session/SegmentInterface.php';
 require_once __DIR__ . '/src/Aura/Session/Segment.php';
 require_once __DIR__ . '/src/Aura/Session/SegmentFactory.php';
+require_once __DIR__ . '/src/Aura/Session/LazySegment.php';
