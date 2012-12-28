71479669883e5527fdca4b95563e7945f12d05f8
Paul M. Jones <pmjones88@gmail.com>
2012-12-27 19:24:43 -0600

    instantiate the manager with $_COOKIE now

--- scripts/instance.php
+++ scripts/instance.php
@@ -2,5 +2,6 @@
 require dirname(__DIR__) . '/src.php';
 return new \Aura\Session\Manager(
     new \Aura\Session\SegmentFactory,
-    new \Aura\Session\CsrfTokenFactory
+    new \Aura\Session\CsrfTokenFactory,
+    $_COOKIE
 );
