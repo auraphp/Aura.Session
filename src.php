e8da74a43d21342615a4a429411c3d9515c737ca
Paul M. Jones <pmjones88@gmail.com>
2012-12-27 20:48:52 -0600

    update src files

--- src.php
+++ src.php
@@ -5,4 +5,3 @@ require_once __DIR__ . '/src/Aura/Session/Manager.php';
 require_once __DIR__ . '/src/Aura/Session/SegmentInterface.php';
 require_once __DIR__ . '/src/Aura/Session/Segment.php';
 require_once __DIR__ . '/src/Aura/Session/SegmentFactory.php';
-require_once __DIR__ . '/src/Aura/Session/LazySegment.php';
