f21675f8b10c9020ebd4b645b8ba9076b4b1dce9
Hari KT <kthari85@gmail.com>
2014-07-18 22:14:04 +0530

    Add license and package defnition

--- src/SessionFactory.php
+++ src/SessionFactory.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ *
+ * This file is part of Aura for PHP.
+ *
+ * @package Aura.Session
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
 namespace Aura\Session;
 
 class SessionFactory
