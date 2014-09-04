b24472b6db4ba55089bee2651d7f42a46fa05e42
Paul M. Jones <pmjones88@gmail.com>
2014-09-04 16:04:47 -0500

    update testing structure and readme

---
+++ tests/container/src/CommonTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Aura\Session\_Config;
+
+use Aura\Di\ContainerAssertionsTrait;
+
+class CommonTest extends \PHPUnit_Framework_TestCase
+{
+    use ContainerAssertionsTrait;
+
+    public function setUp()
+    {
+        $this->setUpContainer(array(
+            'Aura\Session\_Config\Common',
+        ));
+    }
+
+    public function test()
+    {
+        $this->assertGet('session', 'Aura\Session\Session');
+        $this->assertNewInstance('Aura\Session\CsrfTokenFactory');
+        $this->assertNewInstance('Aura\Session\Session');
+        $this->assertNewInstance('Aura\Session\Randval');
+        $this->assertNewInstance('Aura\Session\Segment');
+    }
+}
